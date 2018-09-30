6ae366295fb73e0a16913d8835dd82f59781bd1a
morandj <dominic.moran@gmail.com>
2018-09-30 13:29:08 +0100

    first commit

---
+++ pwa.config.js
@@ -0,0 +1,19 @@
+exports.browsers = [
+  'last 2 Chrome version'
+]
+// exports.workbox = {
+//   advanced: false, // alias
+//   injectManifest: false, // alias
+//   swSrc: 'service-worker.js', // template; mode: InjectManifest
+//   navigateFallbackWhitelist: [/^(?!\/__).*/], // mode: GenerateSW
+//   navigateFallback: 'index.html', // mode: GenerateSW
+//   swDest: 'sw.js',
+//   exclude: [
+//     /\.git/,
+//     /\.map$/,
+//     /\.DS_Store/,
+//     /^manifest.*\.js(?:on)?$/,
+//     /\.gz(ip)?$/,
+//     /\.br$/
+//   ]
+// }
